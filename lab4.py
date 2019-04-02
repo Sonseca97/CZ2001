a57699bb05082c066953ec97f7eefd89a9baf615
zhaochen <zhaochenlf@yahoo.com>
2019-04-02 10:02:32 +0800

    added more cities

--- lab4.py
+++ lab4.py
@@ -1,3 +1,5 @@
+import time
+
 def bfs_shortest(graph, start, destination):
     if(start == destination):
         return "you don't need to take flight!"
@@ -17,23 +19,60 @@ def bfs_shortest(graph, start, destination):
 
 
 if __name__ == '__main__':
-    flight = {
+
+    flight_10 = {
+    'Amsterdam': ['Barcelona', 'London', 'Munich'],
+    'Barcelona': ['Amsterdam', 'Istanbul', 'London'],
+    'Berlin': ['Geneva', 'Munich', 'Warsaw'],
+    'Geneva': ['Berlin', 'Vienna'],
+    'Glasgow': ['Groningen', 'London'],
+    'Groningen': ['Glasgow', 'London', 'Zurich'],
+    'Hamburg': ['Munich', 'Warsaw'],
+    'Munich': ['Hamburg', 'Amsterdam', 'Berlin'],
+    'Istanbul': ['Barcelona', 'London', 'Warsaw'],
+    'London': ['Amsterdam', 'Barcelona', 'Berlin', 'Glasgow', 'Groningen'],
+    }
+
+    flight_15 = {
     'Amsterdam': ['Barcelona', 'London', 'Munich', 'Paris'],
     'Barcelona': ['Amsterdam', 'Istanbul', 'London', 'Paris'],
     'Berlin': ['Geneva', 'Munich', 'Warsaw'],
     'Geneva': ['Berlin', 'Vienna'],
     'Glasgow': ['Groningen', 'London'],
-    'Groningen': ['Glasgow', 'London'],
+    'Groningen': ['Glasgow', 'London', 'Zurich'],
     'Hamburg': ['Munich', 'Warsaw'],
+    'Helsinki': ['Zurich'],
     'Istanbul': ['Barcelona', 'London', 'Paris', 'Warsaw'],
     'London': ['Amsterdam', 'Barcelona', 'Berlin', 'Glasgow', 'Groningen'],
     'Munich': ['Amsterdam', 'Berlin', 'Hamburg', 'Vienna'],
     'Paris': ['Amsterdam', 'Barcelona', 'Istanbul'],
     'Vienna': ['Geneva', 'Munich'],
-    'Warsaw': ['Berlin', 'Hamburg', 'Istanbul']
+    'Warsaw': ['Berlin', 'Hamburg', 'Istanbul'],
+    'Zurich': ['Groningen','Helsinki']
     }
-    start = input("where are you from? ")
-    destination = input("where are you going? ")
-    print(bfs_shortest(flight, start, destination))
 
-
+    flight_20 = {
+    'Amsterdam': ['Barcelona', 'London', 'Munich', 'Paris', 'Prague'],
+    'Barcelona': ['Amsterdam', 'Istanbul', 'London', 'Paris'],
+    'Berlin': ['Geneva', 'Munich', 'Warsaw'],
+    'Beijing': ['Milan'],
+    'Geneva': ['Berlin', 'Vienna'],
+    'Glasgow': ['Groningen', 'London'],
+    'Groningen': ['Glasgow', 'London', 'Zurich'],
+    'Hamburg': ['Munich', 'Warsaw'],
+    'Helsinki': ['Zurich'],
+    'Istanbul': ['Barcelona', 'London', 'Paris', 'Warsaw'],
+    'London': ['Amsterdam', 'Barcelona', 'Berlin', 'Glasgow', 'Groningen', 'Stockholm'],
+    'Milan': ['Beijing', 'Paris'],
+    'Munich': ['Amsterdam', 'Berlin', 'Hamburg', 'Vienna'],
+    'Madrid': ['Paris', 'Zurich'],
+    'Paris': ['Amsterdam', 'Barcelona', 'Madrid', 'Milan','Istanbul', 'Stockholm'],
+    'Prague': ['Amsterdam', 'Vienna'],
+    'Stockholm': ['London', 'Paris'],
+    'Vienna': ['Geneva', 'Munich', 'Prague'],
+    'Warsaw': ['Berlin', 'Hamburg', 'Istanbul'],
+    'Zurich': ['Groningen','Helsinki','Madrid'],
+    }
+    print(time.time())
+    print(bfs_shortest(flight_20, 'Amsterdam', 'Helsinki'))
+    print(time.time())
